6e6d541f2d94ba27a2df83b2350b7dc659a6fa28
cristian.zapata <cristian.zapata@rappi.com>
2024-09-25 00:30:58 -0500

    Componente de Relog Indexado

--- src/app/components/clocks/clocks.component.ts
+++ src/app/components/clocks/clocks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-clocks',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clocks.component.css']
 })
 export class ClocksComponent implements OnInit {
+
+  @Input() index: number = 0;
 
   constructor() { }
 
