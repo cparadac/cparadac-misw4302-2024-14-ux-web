699eca108a57b809195a2efa5ee91e7b6f7489f0
cristian.zapata <cristian.zapata@rappi.com>
2024-09-25 02:58:41 -0500

    Pantalla Listar Categorias

--- src/app/categories/categories-list/categories-list.component.ts
+++ src/app/categories/categories-list/categories-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesListComponent implements OnInit {
 
-  constructor() { }
+  public categories = [
+    {
+      id: 1,
+      name: 'Deportes',
+      description: 'Actividades para mantenerse en forma',
+      clock_style: '2',  
+    },
+    {
+      id: 2,
+      name: 'Tiempo Personal',
+      description: 'Actividades para el tiempo libre',
+      clock_style: '1',
+    },
+    {
+      id: 3,
+      name: 'Trabajo',
+      description: 'Actividades relacionadas con el trabajo',
+      clock_style: '3',
+    },
+    {
+      id: 4,
+      name: 'Salud',
+      description: 'Actividades relacionadas con la salud',
+      clock_style: '2',
+    }
+  ];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log('CategoriesListComponent initialized');
+  }
+
+  edit () {
+    this.router.navigate(['/categories/edit']);
   }
 }
